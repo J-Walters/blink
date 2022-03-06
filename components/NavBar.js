40e7a2ad227f7ab007ba9d8538781290938c6a80
Jordan Walters <jwalters012@gmail.com>
2022-03-05 21:17:33 -0500

    preliminary nav bar

---
+++ components/NavBar.js
@@ -0,0 +1,11 @@
+
+
+export default function NavBar() {
+    return (
+        <nav className="pt-5 border-b-neutral-500 border-b-2 w-11/12 mx-auto">
+            <div className="flex items-center cursor-pointer gap-1">
+                <h1 className="text-[#E5E5E5]">blink</h1>
+            </div> 
+        </nav>
+    )
+} 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        mallanna: ["Mallanna"]
+      }
+    },
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
