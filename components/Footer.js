fcfe3ebad70b945e698f2fda81c12815b20bc58e
Jordan Walters <jwalters012@gmail.com>
2022-03-12 18:35:34 -0500

    favicon and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,20 @@
 import { TiHeartFullOutline } from 'react-icons/ti';
 const Footer = () => {
   return (
-    <div className='flex justify-center border-t-gray-500 border-t-2'>
-      <p className=' flex p-10 text-[#3969A0]'>
-        Made with <TiHeartFullOutline />
-        by Jordan Walters
-      </p>
+    <div className='flex justify-center border-t-[#3969A0] border-t-2'>
+      <div className='flex p-5 text-[#3969A0]'>
+        <p>{`Made with ${'  '}`}</p>
+        <TiHeartFullOutline className='text-red-700' />
+        <p>
+          by{' '}
+          <a
+            href='https://github.com/J-Walters/blink'
+            className='font-bold cursor pointer'
+          >
+            Jordan Walters
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
